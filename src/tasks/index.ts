09f0d1bb5346c332028dff19c8a8f189d522ecb7
ElRaptorus <christian.werner@5minds.de>
2019-02-25 11:22:47 +0100

    :fire: Remove obsolete build scripts If any of them should ever be needed again, they will be contained in a plugin of their own.

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,19 +1,9 @@
 import {generate as build} from './build';
 import {generate as clean} from './clean';
-import {generate as doc} from './doc';
 import {generate as lint} from './lint';
-import {generate as schema} from './schema';
-import {generate as setup_dev} from './setup_dev';
-import {generate as test} from './test';
-import {generate as watch} from './watch';
 
 export function initializePluginTasks(gulp, config, gulptraum) {
   build(gulp, config, gulptraum);
   clean(gulp, config, gulptraum);
-  doc(gulp, config, gulptraum);
   lint(gulp, config, gulptraum);
-  schema(gulp, config, gulptraum);
-  setup_dev(gulp, config, gulptraum);
-  test(gulp, config, gulptraum);
-  watch(gulp, config, gulptraum);
 }
